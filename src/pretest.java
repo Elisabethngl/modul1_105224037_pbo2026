1b37fbe1c9e65204fe9e9239028d7b11c325f268
Elisabeth Nainggolan <sabethngl742@gmail.com>
2026-03-03 14:35:15 +0700

    first commit

---
+++ src/pretest.java
@@ -0,0 +1,9 @@
+public class pretest {
+    public static void main(String[] args) throws Exception {
+       
+
+    System.out.println("==========\nKARTU TANDA MAHASISWA\nNama\t:Elisabeth Nainggolan\nNim\t:105224037\nprodi\t:ilmu komputer ");
+   
+    }
+}
+
